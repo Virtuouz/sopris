d37ae56cd9add2a0059b6bea3981ed131d6c0d0f
Virtuouz <giovanni_plascencia@hotmail.com>
2023-11-23 17:33:00 -0700

    Add new daily build hook

--- netlify/functions/daily-deploy.js
+++ netlify/functions/daily-deploy.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch'
 import { schedule } from '@netlify/functions'
 
-const BUILD_HOOK = 'https://api.netlify.com/build_hooks/651e403439f6323aa4d80fb9'
+const BUILD_HOOK = 'https://api.netlify.com/build_hooks/655fef0fef3d105787c3b2e8'
 
 const handler = schedule('0 9 * * *', async () => {
     await fetch (BUILD_HOOK, {
